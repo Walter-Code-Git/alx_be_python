99901613481b95a0f92db7c44b4c14ac31d635e8
Walter Hlakudi <walterhlakudi@gmail.com>
2025-07-10 23:00:05 +0200

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+# simple_interest.py
+
+# Variable assignments
+principal = 1000      # Initial investment in dollars
+rate = 0.05           # Annual interest rate (5%)
+time = 3              # Investment duration in years
+
+# Calculation using the formula: I = P * R * T
+interest = principal * rate * time
+
+# Output
+print("The simple interest is:", interest)
